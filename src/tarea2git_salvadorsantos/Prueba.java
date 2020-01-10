e9159798e5f7040358fd5987aab91b6f21aabdc2
salvi205 <salvi205s@gmail.com>
2020-01-10 17:43:30 +0100

    Crea un objeto Jugador

---
+++ src/tarea2git_salvadorsantos/Prueba.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tarea2git_salvadorsantos;
+
+/**
+ *
+ * @author Salva
+ */
+public class Prueba {
+    
+    public static void main(String[] args) {
+        
+        Jugador jugador=new Jugador("Pepe", 5, 1);
+    }
+}
